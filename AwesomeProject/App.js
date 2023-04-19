8558ba51d9e8a6530c6b26166b8619b192220e09
Adam Hocini <adam.hocini.ah@icloud.com>
2023-04-19 09:56:02 +0200

    ReactNativeDay03.1

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,14 +1,24 @@
-import { React, useState} from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { React, useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
+
   const handleAddTask = () => {
+    Keyboard.dismiss();
     setTaskItems([...taskItems, task])
     setTask(null);
   }
+
+{/*splice = êrmet de supprimer*/}
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
 
@@ -17,10 +27,14 @@ export default function App() {
         <Text style={styles.tasksTitle}>Todo List</Text>
 
         <View style={styles.items}>
-          {/*les tasks*/}
+          
           {
-            taskItems.map((item, index) =>{
-              return <Task key= {index} text= {item} />
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={()=> completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
             })
           }
         </View>
@@ -30,7 +44,7 @@ export default function App() {
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={'Ecrivez une tache'} value ={task} onChangeText={text =>setTask(text)} />
+        <TextInput style={styles.input} placeholder={'Ecrivez une tache'} value={task} onChangeText={text => setTask(text)} />
 
         <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
@@ -73,7 +87,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     borderColor: '#C0C0C0',
     borderWidth: 1,
-    width:250,
+    width: 250,
   },
   addWrapper: {
     width: 60,
