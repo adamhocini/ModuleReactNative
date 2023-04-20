ccf19bfe8baae66b90a86909fb22d2649be9c62d
Adam Hocini <adam.hocini.ah@icloud.com>
2023-04-20 09:38:46 +0200

    Day04

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,7 +1,8 @@
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import  Settings from './screens/Settings';
+import Settings from './screens/Settings';
+import Profil from './screens/Profil';
 
 const Stack = createNativeStackNavigator()
 
